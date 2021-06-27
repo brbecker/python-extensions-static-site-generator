3042d4be4a4f4f8d92d403ce3b38d896401984a4
Brian R. Becker <brbecker@gmail.com>
2021-06-27 06:58:25 +0000

    Load Extensions

---
+++ ssg/extensions.py
@@ -0,0 +1,20 @@
+import importlib
+from os import DirEntry
+import sys
+from pathlib import Path
+
+
+def load_module(directory: str, name: str) -> None:
+    sys.path.insert(0, directory)
+    importlib.import_module(name)
+    sys.path.pop(0)
+
+
+def load_directory(directory: Path) -> None:
+    for path in directory.rglob("*.py"):
+        load_module(directory.as_posix(), path.stem)
+
+
+def load_bundled() -> None:
+    directory = Path(__file__).parent / "extensions"
+    load_directory(directory)
